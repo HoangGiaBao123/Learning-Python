e214afb738b45d180122881bab61bfcc49bfa913
Gia Bảo <hoanggiabao131012vn@gmail.com>
2026-02-12 12:41:17 +0700

    Create power.py

---
+++ power.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    a = int(input("Enter 1st num: "))
+    b = int(input("Enter 2nd num: "))
+    m = int(input("Enter 3rd num: "))
+    print(pow(a, b))
+    print(pow(a, b, m))
