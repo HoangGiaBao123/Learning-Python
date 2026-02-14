2988cb39e6cb49560b9e25a4e549ccc53b79533c
Gia Bảo <hoanggiabao131012vn@gmail.com>
2026-02-14 19:47:39 +0700

    Create Divmod().py

---
+++ Divmod().py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a // b)
+    print(a % b)
+    
+    print(divmod(a, b))
+    # This is divmod() function
+    # Divmod() function wil return 2 values
+    # 1st value: a // b
+    # 2nd value a % b
